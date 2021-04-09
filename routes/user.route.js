eaac6b06c635ca8a14990ba89a4dd9e35217c5c1
praveennick <praveennick1@gmail.com>
2021-04-09 11:10:15 +0530

    added some apis

---
+++ routes/user.route.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var userController = require('../controllers/user.controller');
+
+var userRouter= express.Router();
+
+//http://localhost:9545/register
+userRouter.post('/register',userController.registerUser);
+
+//http://localhost:9545/login
+userRouter.post('/login',userController.loginUser);
+
+//http://localhost:9545/allUsers
+userRouter.get('/allUsers',userController.getAllUsers);
+
+//http://localhost:9545/getUserById/
+userRouter.get('/getUserById/:s_id',userController.getUserById);
+
+module.exports=userRouter;
