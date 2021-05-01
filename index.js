1db88ce906541e978a9f9935dd89227259853a40
praveennick <praveennick1@gmail.com>
2021-05-01 11:59:07 +0530

    added cors

--- index.js
+++ index.js
@@ -12,9 +12,23 @@ DB.connect();
 
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" );
+    next();
+  });
 
 //app.use('/admin',userRouter);
 app.use(userRouter);
+
+app.get('/',(req,res)=>{
+    res.send("Welcome to University Backend Application");
+    
+})
 
 app.listen(9545,function(){
     console.log("Server started!")
